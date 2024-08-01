5b02938e32bc7691c1e3ae9940c450ec4924a9eb
Kirk Silva <kirk.ki@hotmail.com>
2024-08-01 10:48:41 -0300

    git init.

---
+++ src/main/java/kirk/example/java_api_dio/App.java
@@ -0,0 +1,13 @@
+package kirk.example.java_api_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ src/test/java/kirk/example/java_api_dio/JavaApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package kirk.example.java_api_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
